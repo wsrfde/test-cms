c0b52bb7a35c70573508d55f5662a053b12dc7b9
Viceroy <wsrf.liu@qq.com>
2022-05-22 17:25:38 +0800

    fix(修复config): 更改为dist文件

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,12 @@ const path = require('path')
 
 module.exports = {
   // 1.配置方式一: CLI提供的属性
-  outputDir: './build',
+  outputDir: './dist',
   // publicPath: './',
   devServer: {
     proxy: {
       '^/api': {
-        target: 'http://152.136.185.210:5000',
+        target: 'http://152.136.185.210:4000',
         pathRewrite: {
           '^/api': ''
         },
